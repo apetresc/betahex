d59609112fa5cb409d52dafa60eb19889b8843ab
Adrian Petrescu <adrian@apetre.sc>
2023-04-01 16:40:15 -0400

    Add --board-size flag to bot_v_bot.py

--- bot_v_bot.py
+++ bot_v_bot.py
@@ -1,13 +1,12 @@
+import argparse
+
 from dlhex.agent import naive
 from dlhex.hexboard import GameState
 from dlhex.hextypes import Player
 from dlhex.utils import print_diamond_board, print_move
 
-import time
 
-
-def main():
-    board_size = 13
+def main(board_size: int = 13):
     game = GameState.new_game(board_size)
     bots = {
         Player.black: naive.RandomBot(),
@@ -19,8 +18,11 @@ def main():
         game = game.apply_move(bot_move)
         print_diamond_board(game.board)
         print_move(game.next_player.other, bot_move)
-        time.sleep(0.3)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--board-size', '-n', type=int, default=13)
+    args = parser.parse_args()
+
+    main(args.board_size)
